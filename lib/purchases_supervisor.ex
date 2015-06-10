a1a49a3a9a4cbd98953c69e388adab425c08f65f
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-06-09 17:56:20 -0700

    This supervisor governs the other managers that handle Purchase Data workers / and sub supervisors

--- lib/purchases_supervisor.ex
+++ lib/purchases_supervisor.ex
@@ -2,12 +2,19 @@ defmodule PurchasesSupervisor do
   use Supervisor
 
   def start_link do 
-    :supervisor.start_link(__MODULE__, [])
+    result = {:ok, sup} = :supervisor.start_link( __MODULE__, [])
+    start_workers( sup )
+    result
   end
 
-  def init(purchases) do 
-    # child processes
-    children = [worker(PurchasesServer, purchases)]
-    supervise children, strategy: :one_for_one
+  def start_workers( sup ) do 
+    # Start the PurchaseData worker.
+    {:ok, purchase_data_pid } = :supervisor.start_child( sup, worker( Core.PurchaseData, []))
+    # Start Sub supervisor
+    :supervisor.start_child( sup, worker(Core.PurchasesSupervisorSub, [ purchase_data_pid ]))
+  end
+
+  def init(_) do 
+    supervise [], strategy: :one_for_one
   end
 end
