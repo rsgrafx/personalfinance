db70a76878353bcf98103aad8df587c106d252be
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-06-16 22:54:25 -0700

    corrected postgres configuration

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,7 @@
 use Mix.Config
   config :banking, Core.BankingRepo,
     adapter: Ecto.Adapters.Postgres,
-    database: "personal_banking_app",
-    username: "root",
-    password: ""
+    database: "personal_banking_income",
+    username: "orionengleton",
+    password: "",
+    hostname: "127.0.0.1"
