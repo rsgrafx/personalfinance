34ecf56a2ba610ccd9c1ccfdbb45f21507fd1c9e
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-06-16 22:57:42 -0700

    Passing the data from redis into the postgres table now - fixed the function to utilize provided ecto struct Models.PurchaseItem

--- lib/pubsub/msg_pusher.ex
+++ lib/pubsub/msg_pusher.ex
@@ -2,15 +2,14 @@ defmodule Pubsub.MsgPusher do
   @doc """
   This module captures the data from the original connection // Memory store -> Persistent Posgres
   # Expects json from redis 
-  PUBLISH ocr_data '{"item_description" : "longtime", "item_tags" : "food, groceries", "price": "22.32"}'
+  PUBLISH ocr_data '{"description" : "Sprouts grocesy store receipt", "item_tags" : "food, groceries", "price": "22.32"}'
   """
   def send(msg) do
     case msg do
       {:message, _, extracted_msg, _} ->
         IO.puts "#{log_time} [REDIS MESSGE] #{inspect msg}"
-        extracted_msg |> convert_to_struct
+        extracted_msg |> convert_to_struct |> Db.Postgres.save_message
         IO.puts "#{log_time} [REDIS MESSGE] #{inspect extracted_msg}"
-        # |> Postgres.Db.save_message(extracted_msg) * save struct to db.
       _ -> # nada
     end
   end
@@ -21,6 +20,6 @@ defmodule Pubsub.MsgPusher do
   end
 
   defp convert_to_struct(msg) do 
-    Poison.decode!( msg , as: Core.PurchaseItem)
+    Poison.decode!( msg , as: Models.PurchaseItem )
   end
 end
