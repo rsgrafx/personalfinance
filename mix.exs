5b9fc531e17f97d9936da2724b3b928baa02c83a
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-06-19 22:12:26 -0700

    Added plug, cowbow and removed tag for exredis - new support for multi channels just added

--- mix.exs
+++ mix.exs
@@ -2,23 +2,27 @@ defmodule PersonalBanking.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :personal_banking,
+    [
+     app: :personal_banking,
      version: "0.0.1",
      elixir: "~> 1.0",
-     deps: deps]
+     deps: deps
+    ]
   end
 
   def application do
-    [applications: [:logger,:postgrex, :ecto]]
+    [applications: [:logger,:postgrex, :ecto, :cowboy], mod: {PersonalBanking, []} ]
   end
 
   defp deps do
     [
      {:timex, "~> 0.13.4" },
-     {:exredis, github: "artemeff/exredis", tag: "0.1.0"},
+     {:exredis, github: "artemeff/exredis"},
      {:postgrex, "~> 0.7.0"},
      {:ecto, "0.7.1"},
-     {:poison, github: "devinus/poison"}
+     {:poison, github: "devinus/poison"},
+     {:plug, github: "elixir-lang/plug"},
+     {:cowboy, "~> 1.0.0"}
    ]
   end
 end
