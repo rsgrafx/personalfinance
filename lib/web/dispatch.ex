5ac0318f78cd468effe54ae393df0c49126d05c4
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-06-20 03:02:04 -0700

    moved routes list to a function - removed un used routes

--- lib/web/dispatch.ex
+++ lib/web/dispatch.ex
@@ -1,17 +1,16 @@
 defmodule Web.Dispatch do 
   def start do 
-    dispatch = :cowboy_router.compile([
-      {:_,
-        [
-          {"/", Web.PageHandler, []},
-          {"/websocket", Web.Realtime, []},
-          {"/second_socket", Web.UploadUpdates, []},
-          {"/[...]", :cowboy_static, { :priv_dir, :personal_banking, "", 
-            [{:mimetypes, :cow_mimetypes, :all}]}}
-          ]} ])
+    dispatch = :cowboy_router.compile([ {:_, routes} ] )
 
     {:ok, _} = :cowboy.start_http(:personal_banking, 100, [{:port, 8090}],
-    [{:env, [{:dispatch, dispatch}]}]
-      )
+    [{:env, [{:dispatch, dispatch}]}])
+  end
+
+  def routes do 
+        [
+          {"/websocket", Web.Realtime, []},
+          {"/second_socket", Web.UploadUpdates, []},
+          {"/[...]", :cowboy_static, { :priv_dir, :personal_banking, "", [{:mimetypes, :cow_mimetypes, :all}]}}
+        ]
   end
 end
