322c8e075bd71466f7c4972303a3e0622f9b7740
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-06-19 22:13:24 -0700

    Added supervisors directly to this file - will move out to a lib/supervisors folder later

--- lib/core/purchases_supervisor_sub.ex
+++ lib/core/purchases_supervisor_sub.ex
@@ -13,3 +13,35 @@ defmodule Core.PurchasesSupervisorSub do
     supervise child_prx, strategy: :one_for_one
   end
 end
+
+defmodule Core.RedisSupervisorSub do
+  @doc """
+  This supervisor manages the Redis Supervisor application / process.
+  """
+  use Supervisor
+
+  def start_link( pid ) do 
+    :supervisor.start_link( __MODULE__, pid )
+  end
+
+  def init( pid ) do 
+    child_prx = [ worker(Pubsub.Connection, [ pid ] ) ]
+    supervise child_prx, strategy: :one_for_one
+  end
+end
+
+defmodule Core.PostgresSupervisorSub do
+  @doc """
+  This supervisor manages the Postgres Supervisor application / process.
+  """
+  use Supervisor
+
+  def start_link( pid ) do 
+    :supervisor.start_link( __MODULE__, pid )
+  end
+
+  def init( pid ) do 
+    child_prx = [ worker(Core.BankingRepo, [ pid ] ) ]
+    supervise child_prx, strategy: :one_for_one
+  end
+end
