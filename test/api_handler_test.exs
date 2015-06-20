91b18cfe386df62dc10b847ae603a508911a566e
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-06-20 03:06:13 -0700

    will comeback to handle this test

---
+++ test/api_handler_test.exs
@@ -0,0 +1,19 @@
+defmodule Web.ApiHanlderTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  @opts Web.ApiHandler.init([])
+
+  test "returns hello world" do
+    # Create a test connection
+    conn = conn(:get, "/hello")
+
+    # Invoke the plug
+    conn = Web.ApiHandler.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "world"
+  end
+end
